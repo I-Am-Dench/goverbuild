dba5934b681c28afab7af7203307bbc0f2c06e2c
Theodore <mrantistupid@gmail.com>
2024-10-28 17:35:46 -0400

    Removed old reader.go and add Unwrap method to RecordError

--- archive/catalog/errors.go
+++ archive/catalog/errors.go
@@ -1,6 +1,8 @@
 package catalog
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type RecordError struct {
 	Err   error
@@ -11,24 +13,6 @@ func (err *RecordError) Error() string {
 	return fmt.Sprintf("record: %s: %s", err.Field, err.Err.Error())
 }
 
-// type ReadFileError struct {
-// 	Err   error
-// 	Field string
-// }
-
-// func (err *ReadFileError) Error() string {
-// 	return fmt.Sprintf("catalog: file: %s: %s", err.Field, err.Err.Error())
-// }
-
-// func (err *ReadFileError) Unwrap() error {
-// 	return err.Err
-// }
-
-// func (err *ReadFileError) Is(target error) bool {
-// 	switch target.(type) {
-// 	case *ReadFileError:
-// 		return true
-// 	default:
-// 		return errors.Is(err, target)
-// 	}
-// }
+func (err *RecordError) Unwrap() error {
+	return err.Err
+}
